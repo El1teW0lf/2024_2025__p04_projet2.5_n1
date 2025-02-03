e4a9d40dfdf5c73ba07a1a07a78c0bc3dbe76a86
Herasium <soubrievictor@gmail.com>
2025-02-03 09:53:56 +0100

    cicular offofo

--- __main__.py
+++ __main__.py
@@ -1,27 +1,24 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+from modules.CustomFPC import CustomFirstPersonController
 
-app = Ursina()
 
-nsi_room =  load_blender_scene('nsi_room_def1',application.asset_folder)
+app = Ursina(development_mode=False)
 
-nsi_room.Plane_006.collider = 'box'
-nsi_room.Plane_006.visible = True
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
-player = FirstPersonController(model='cube', z=-10, origin_y=-.5, speed=8, collider='box')
-player.fade_out(0,0)
-player.set_position(Vec3(10.76,3.6,9.55))
+player = CustomFirstPersonController(model='cube', z=-10, origin_y=0, speed=8, collider='box')
+player.set_position(Vec3(0,0,0))
 player.speed = 0
 player.gravity = 0
 
+e = Entity(model='cube', color=color.orange, position=(0,1,5), scale=5, rotation=(0,0,0), texture='fnaf_office.jpg')
+
 def pause_input(key):
-    if key == 'tab':    
+    if key == 'escape':    
 
         editor_camera.enabled = not editor_camera.enabled
         player.visible_self = editor_camera.enabled
-        player.cursor.enabled = not editor_camera.enabled
-        mouse.locked = not editor_camera.enabled
         editor_camera.position = player.position
         application.paused = editor_camera.enabled
 
---
+++ modules/CustomFPC.py
@@ -0,0 +1,85 @@
+from ursina import *
+
+class CustomFirstPersonController(Entity):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.speed = 5
+        self.height = 1
+        self.camera_pivot = Entity(parent=self, y=self.height)
+
+        camera.parent = self.camera_pivot
+        camera.position = (0,0,0)
+        camera.rotation = (0,0,0)
+        camera.fov = 90
+        mouse.locked = False
+        self.mouse_sensitivity = Vec2(40, 40)
+
+        self.gravity = 1
+        self.grounded = False
+        self.jump_height = 2
+        self.jump_up_duration = .5
+        self.fall_after = .35 # will interrupt jump up
+        self.jumping = False
+        self.air_time = 0
+
+        self.traverse_target = scene     # by default, it will collide with everything. change this to change the raycasts' traverse targets.
+        self.ignore_list = [self, ]
+        self.on_destroy = self.on_disable
+
+        for key, value in kwargs.items():
+            setattr(self, key ,value)
+
+        # make sure we don't fall through the ground if we start inside it
+        if self.gravity:
+            ray = raycast(self.world_position+(0,self.height,0), self.down, traverse_target=self.traverse_target, ignore=self.ignore_list)
+            if ray.hit:
+                self.y = ray.world_point.y
+
+
+    def update(self):
+        self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
+
+        #self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
+        #self.camera_pivot.rotation_x= clamp(self.camera_pivot.rotation_x, -90, 90)
+
+        self.direction = Vec3(
+            self.forward * (held_keys['w'] - held_keys['s'])
+            + self.right * (held_keys['d'] - held_keys['a'])
+            ).normalized()
+
+        feet_ray = raycast(self.position+Vec3(0,0.5,0), self.direction, traverse_target=self.traverse_target, ignore=self.ignore_list, distance=.5, debug=False)
+        head_ray = raycast(self.position+Vec3(0,self.height-.1,0), self.direction, traverse_target=self.traverse_target, ignore=self.ignore_list, distance=.5, debug=False)
+        if not feet_ray.hit and not head_ray.hit:
+            move_amount = self.direction * time.dt * self.speed
+
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(1,0,0), distance=.5, traverse_target=self.traverse_target, ignore=self.ignore_list).hit:
+                move_amount[0] = min(move_amount[0], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(-1,0,0), distance=.5, traverse_target=self.traverse_target, ignore=self.ignore_list).hit:
+                move_amount[0] = max(move_amount[0], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(0,0,1), distance=.5, traverse_target=self.traverse_target, ignore=self.ignore_list).hit:
+                move_amount[2] = min(move_amount[2], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(0,0,-1), distance=.5, traverse_target=self.traverse_target, ignore=self.ignore_list).hit:
+                move_amount[2] = max(move_amount[2], 0)
+            self.position += move_amount
+
+
+
+
+
+    def input(self, key):
+        if key == 'space':
+            pass
+
+
+    def on_enable(self):
+        mouse.locked = False
+        if hasattr(self, 'camera_pivot') and hasattr(self, '_original_camera_transform'):
+            camera.parent = self.camera_pivot
+            camera.transform = self._original_camera_transform
+
+
+    def on_disable(self):
+        mouse.locked = False
+        self._original_camera_transform = camera.transform  
+        camera.world_parent = scene
+
